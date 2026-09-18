2793b508a9ec7342ab1e6a6387381ff5d032385f
Sathish Kumar <sathishspark513@gmail.com>
2026-09-18 15:23:42 +0530

    Configure MySQL SSL for Aiven

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -74,7 +74,10 @@ else:
             "PASSWORD": os.getenv("DB_PASSWORD") or "",
             "HOST": os.getenv("DB_HOST", "127.0.0.1"),
             "PORT": os.getenv("DB_PORT", "3306"),
-            "OPTIONS": {"charset": "utf8mb4"},
+            "OPTIONS": {
+                "charset": "utf8mb4",
+                "ssl": {},
+            },
         }
     }
 
